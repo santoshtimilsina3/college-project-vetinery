a3d6f005e501237910359147f60081ab09d3e012
santosh timilsina <santos.timil@gmail.com>
2023-08-09 11:39:37 +0545

    Generic-Response-made-to-send-success-response

---
+++ Veterinary-Web-Application-SpringBoot-Security/src/main/java/com/example/demo/dto/response/ResponseBuilder.java
@@ -0,0 +1,65 @@
+package com.example.demo.dto.response;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class ResponseBuilder {
+
+    public ResponseEntity<ApiResponse> buildResponse(
+            HttpHeaders httpHeaders, int httpStatusCode, String message, Object data, Map<String, Object> otherParams) {
+        return new ApiResponse.ApiResponseBuilder<>(httpStatusCode, message)
+                .withHttpHeaders(httpHeaders)
+                .withData(data)
+                .withOtherParams(otherParams)
+                .build();
+    }
+
+    public ResponseEntity<ApiResponse> buildResponse(
+            int httpStatusCode, String message, Object data, Map<String, Object> otherParams) {
+        return new ApiResponse.ApiResponseBuilder<>(httpStatusCode, message)
+                .withData(data)
+                .withOtherParams(otherParams)
+                .build();
+    }
+
+    public ResponseEntity<ApiResponse> buildResponse(
+            int httpStatusCode, String message, Map<String, Object> otherParams) {
+        return new ApiResponse.ApiResponseBuilder<>(httpStatusCode, message)
+                .withOtherParams(otherParams)
+                .build();
+    }
+
+    public ResponseEntity<ApiResponse> buildResponse(
+            HttpHeaders httpHeaders, int httpStatusCode, String message, Object data) {
+        return new ApiResponse.ApiResponseBuilder<>(httpStatusCode, message)
+                .withHttpHeaders(httpHeaders)
+                .withData(data)
+                .build();
+    }
+
+    public ResponseEntity<ApiResponse> buildResponse(
+            HttpHeaders httpHeaders, int httpStatusCode, String message, Map<String, Object> otherParams) {
+        return new ApiResponse.ApiResponseBuilder<>(httpStatusCode, message)
+                .withHttpHeaders(httpHeaders)
+                .withOtherParams(otherParams)
+                .build();
+    }
+
+    public ResponseEntity<ApiResponse> buildResponse(
+            HttpHeaders httpHeaders, int httpStatusCode, String message) {
+        return new ApiResponse.ApiResponseBuilder<>(httpStatusCode, message)
+                .withHttpHeaders(httpHeaders)
+                .build();
+    }
+
+    public ResponseEntity<ApiResponse> buildResponse(
+            int httpStatusCode, String message, Object data) {
+        return new ApiResponse.ApiResponseBuilder<>(httpStatusCode, message)
+                .withData(data)
+                .build();
+    }
+}
